d9576f64c21e0f3404c164ef25f1c7783bdd8599
mikyyt <44488220+mikyyt@users.noreply.github.com>
2019-10-30 11:36:11 -0500

    tercera modificacion web
    + string sms

--- src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
+++ src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
@@ -5,5 +5,8 @@ package pe.edu.idat.daa2.segundoproyecto;
 public class App {
     public static void main(String[] args) {
         System.out.println("pe.edu.idat.daa2.segundoproyecto.App.main()");
+        
+        String sms="modificacion desde web";
+        
     }
 }
