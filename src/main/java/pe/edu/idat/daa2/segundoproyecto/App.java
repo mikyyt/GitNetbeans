fd2b7293bd61b71f0a5f7d666a99851f0e52ec9c
Miguel <a1715981@idat.edu.pe>
2019-10-30 11:34:48 -0500

    + Segunda version desde Netbeans

--- src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
+++ src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
@@ -4,6 +4,6 @@ package pe.edu.idat.daa2.segundoproyecto;
 
 public class App {
     public static void main(String[] args) {
-        
+        System.out.println("pe.edu.idat.daa2.segundoproyecto.App.main()");
     }
 }
